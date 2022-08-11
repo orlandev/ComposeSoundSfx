194d24daacdef13cb4428891be364fcedfbebd5d
Orlando N. Rodriguez <onovas05@gmail.com>
2022-08-10 21:19:26 -0400

    Some changes

--- app/src/main/java/com/orlandev/composesoundsfx/SoundSfx.kt
+++ app/src/main/java/com/orlandev/composesoundsfx/SoundSfx.kt
@@ -1,12 +1,15 @@
 package com.orlandev.composesoundsfx
 
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,8 +17,6 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import kotlin.random.Random
 
 
 @Composable
@@ -24,15 +25,18 @@ fun SoundSfx(
     size: Size = Size(100f, 200f),
     barCount: Int = 10,
 ) {
-
+    val listOffset = remember {
+        mutableStateOf<List<Float>>(generateList(barCount))
+    }
     //Animation - it will be repeated 2 times
     val animatedProgress = remember { Animatable(0.001f) }
     LaunchedEffect(animatedProgress) {
+
         animatedProgress.animateTo(
             1f,
             animationSpec = infiniteRepeatable(
                 animation = tween(
-                    durationMillis = 300,
+                    durationMillis = 1000,
                     easing = LinearEasing
                 )
             )
@@ -47,34 +51,39 @@ fun SoundSfx(
             constraints.maxWidth / (2f * barCount)
         }
 
+
+
         Canvas(modifier = Modifier.fillMaxSize()) {
             val startingPoint = Offset(0f, size.height / 2f)
             val endingPoint = Offset(size.width * animatedProgress.value, size.height / 2f)
-
+            listOffset.value = generateList(barCount)
             //At the end of animation just remove the line
             if (animatedProgress.isRunning) {
-               /* drawLine(
-                    strokeWidth = 8.dp.toPx(),
-                    color = Color.Black,
-                    start = startingPoint,
-                    end = endingPoint
-                )*/
-
                 for (i in 0 until barCount) {
                     drawRoundRect(
                         color = Color.Black,
                         topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
-                        size = Size(barWidth, endingPoint.x.toFloat()),
+                        size = Size(barWidth, size.width * listOffset.value[i]),
+                        cornerRadius = CornerRadius(0f)
+                    )
+                    drawRoundRect(
+                        color = Color.Black,
+                        topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
+                        size = Size(barWidth, size.width * (-listOffset.value[i])),
                         cornerRadius = CornerRadius(0f)
                     )
                 }
-
             }
-
-            ///---------
-
-
         }
     }
+}
 
+
+//Function to generate random float list
+fun generateList(size: Int): List<Float> {
+    val list = mutableListOf<Float>()
+    for (i in 0 until size) {
+        list.add(Math.random().toFloat())
+    }
+    return list
 }
